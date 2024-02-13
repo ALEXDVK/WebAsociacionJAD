474fd3b0cda30a64eb82bf547b902bf7069525e5
ALEXDVK <afrancoyuste@gmail.com>
2024-02-13 17:40:15 +0100

    estilos/js y registro modificado version 2.1

--- javascript.js
+++ javascript.js
@@ -1,23 +1,33 @@
-window.addEventListener("load", function() {
-
-    // icono para mostrar contraseña
-    showPassword = document.querySelector('.show-password');
-    showPassword.addEventListener('click', () => {
-
-        // elementos input de tipo clave
-        password1 = document.querySelector('.password1');
-        password2 = document.querySelector('.password2');
-
-        if ( password1.type === "text" ) {
-            password1.type = "password"
-            password2.type = "password"
-            showPassword.classList.remove('fa-eye-slash');
-        } else {
-            password1.type = "text"
-            password2.type = "text"
-            showPassword.classList.toggle("fa-eye-slash");
-        }
-
-    })
-
-});
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    var nombre = document.getElementById('usuario').value;
+    if(nombre.length == 0) {
+      alert('No has escrito nada en el nombre');
+      return;
+    }
+    var apellido = document.getElementById('apellido').value;
+    if(apellido.length == 0) {
+      alert('No has escrito nada en el apellido');
+      return;
+    }
+    var DNI = document.getElementById('DNI').value;
+    if(DNI.length < 9) {
+      alert('No es un DNI válido');
+      return;
+    }
+    var clave = document.getElementById('contraseña').value;
+    if (clave.length  < 5 ) {
+      alert('La contraseña debe tener un mínimo de 5 caracteres');
+      return;
+    }
+    var telefono = document.getElementById('tel').value;
+    if(telefono.length < 9) {
+      alert('No es un teléfono válido');
+      return;
+    }
+    this.submit();
+  }
